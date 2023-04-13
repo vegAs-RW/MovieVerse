f69fa0031915fee17d6a10db6d3a5ab24552d03f
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 16:42:20 +0200

    add props on Header component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
     <Container>
       <BackgroundImg />
       <div className="content">
-        <Header />
+        <Header login/>
         <div className="body flex column a-center j-center">
           <div className="text flex column">
             <h1>Unlimited movies, TV shows and more</h1>
